1c67930b15f0bb2af81b6773822f667857dcecbd
dku124 <duongkhanhuyen124@gmail.com>
2022-09-09 15:21:29 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-$(".content_list.portfolio li").fancybox({
-  // Options will go here
+//fancybox
+$("[data-fancybox]").fancybox({
   buttons: ["close"],
   wheel: false,
   transitionEffect: "slide",
-  // thumbs          : false,
-  // hash            : false,
   loop: true,
-  // keyboard        : true,
   toolbar: false,
-  // animationEffect : false,
-  arrows: true,
   clickContent: false,
 });
