d8188844877a04e9e727d5307a30cf5722f89409
dku124 <duongkhanhuyen124@gmail.com>
2022-09-09 14:14:59 +0700

    responsive and fancybox

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(".content_list.portfolio li").fancybox({
+  // Options will go here
+  buttons: ["close"],
+  wheel: false,
+  transitionEffect: "slide",
+  // thumbs          : false,
+  // hash            : false,
+  loop: true,
+  // keyboard        : true,
+  toolbar: false,
+  // animationEffect : false,
+  arrows: true,
+  clickContent: false,
+});
